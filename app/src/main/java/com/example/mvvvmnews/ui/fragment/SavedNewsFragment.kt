be9cf398fb784e2c12143de3b4c8a3b02ae03a6a
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-23 15:34:12 +0530

    fragment layout passed

--- app/src/main/java/com/example/mvvvmnews/ui/fragment/SavedNewsFragment.kt
+++ app/src/main/java/com/example/mvvvmnews/ui/fragment/SavedNewsFragment.kt
@@ -1,8 +1,9 @@
 package com.example.mvvvmnews.ui.fragment
 
 import androidx.fragment.app.Fragment
+import com.example.mvvvmnews.R
 
 
-class SavedNewsFragment : Fragment() {
+class SavedNewsFragment : Fragment(R.layout.fragment_saved_news) {
 
 }
