0d41e1f5a5b494a8b1e8cc7484bebd75f844f8c1
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-23 19:16:28 +0530

    extended class with RoomDatabase()

--- app/src/main/java/com/example/mvvvmnews/ui/db/ArticleDatabase.kt
+++ app/src/main/java/com/example/mvvvmnews/ui/db/ArticleDatabase.kt
@@ -1,9 +1,10 @@
 package com.example.mvvvmnews.ui.db
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.example.mvvvmnews.ui.models.Article
 
 @Database(entities = [Article::class], version = 1)
-abstract class ArticleDatabase {
+abstract class ArticleDatabase : RoomDatabase() {
 
 }
