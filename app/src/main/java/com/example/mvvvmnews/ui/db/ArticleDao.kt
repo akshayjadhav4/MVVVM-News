b95a675889f760ec800437e42ab24e02a78dc4d0
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-23 19:06:08 +0530

    added function to insert and update article in database. it returns Id

--- app/src/main/java/com/example/mvvvmnews/ui/db/ArticleDao.kt
+++ app/src/main/java/com/example/mvvvmnews/ui/db/ArticleDao.kt
@@ -1,7 +1,13 @@
 package com.example.mvvvmnews.ui.db
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import com.example.mvvvmnews.ui.Article
 
 @Dao
 interface ArticleDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(article: Article): Long
 }
