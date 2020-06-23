7361278b746c4764eb579debb0b3485f7d76bcbc
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-23 18:32:49 +0530

    returned response

--- app/src/main/java/com/example/mvvvmnews/ui/api/NewsApi.kt
+++ app/src/main/java/com/example/mvvvmnews/ui/api/NewsApi.kt
@@ -1,6 +1,8 @@
 package com.example.mvvvmnews.ui.api
 
+import com.example.mvvvmnews.ui.NewsResponse
 import com.example.mvvvmnews.ui.util.Constants.Companion.API_KEY
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -16,5 +18,5 @@ interface NewsApi {
         pageNumber: Int = 1,
         @Query("apiKey")
         apiKey: String = API_KEY
-    )
+    ): Response<NewsResponse>
 }
