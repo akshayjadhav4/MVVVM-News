c239a153ea1644b359ed69069ab3e6d81791fb2e
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-23 19:08:24 +0530

    added function to get all saved articles

--- app/src/main/java/com/example/mvvvmnews/ui/db/ArticleDao.kt
+++ app/src/main/java/com/example/mvvvmnews/ui/db/ArticleDao.kt
@@ -1,8 +1,10 @@
 package com.example.mvvvmnews.ui.db
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.example.mvvvmnews.ui.Article
 
 @Dao
@@ -10,4 +12,7 @@ interface ArticleDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun upsert(article: Article): Long
+
+    @Query("SELECT * FROM articles")
+    fun getAllArticles(): LiveData<List<Article>>
 }
