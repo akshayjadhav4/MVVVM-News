b56e64574aa46e0920aacf3c661352f7f4e00f4b
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-23 17:46:24 +0530

    connected bottomNavigation with navController

--- app/src/main/java/com/example/mvvvmnews/ui/NewsActivity.kt
+++ app/src/main/java/com/example/mvvvmnews/ui/NewsActivity.kt
@@ -2,12 +2,18 @@ package com.example.mvvvmnews.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.mvvvmnews.R
+import kotlinx.android.synthetic.main.activity_news.*
 
 class NewsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
+
+        //connecting bottom navigation
+        bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
     }
 }
