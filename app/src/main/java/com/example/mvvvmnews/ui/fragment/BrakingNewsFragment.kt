6f24811156f149c0dfaf1ef4ea64ceec5c6738f0
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-23 15:29:42 +0530

    fragment layout passed to constructor

--- app/src/main/java/com/example/mvvvmnews/ui/fragment/BrakingNewsFragment.kt
+++ app/src/main/java/com/example/mvvvmnews/ui/fragment/BrakingNewsFragment.kt
@@ -1,9 +1,9 @@
 package com.example.mvvvmnews.ui.fragment
 
 import androidx.fragment.app.Fragment
+import com.example.mvvvmnews.R
 
 
-
-class BrakingNewsFragment : Fragment() {
+class BrakingNewsFragment : Fragment(R.layout.fragment_braking_news) {
 
 }
