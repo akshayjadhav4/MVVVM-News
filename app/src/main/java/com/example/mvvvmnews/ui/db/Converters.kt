92547e4c1637ba2b7e80cfd3bf6e0a03ce2771fd
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-23 19:32:05 +0530

    defining TypeConverter to convert string type to source

--- app/src/main/java/com/example/mvvvmnews/ui/db/Converters.kt
+++ app/src/main/java/com/example/mvvvmnews/ui/db/Converters.kt
@@ -10,4 +10,9 @@ class Converters {
     fun fromSource(source: Source): String {
         return source.name
     }
+
+    @TypeConverter
+    fun toSource(name: String): Source {
+        return Source(name,name)
+    }
 }
