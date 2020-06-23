47d119183cc4977e12b6f7c7d6e8ac7a12563c79
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-23 18:31:38 +0530

    passed Query Params

--- app/src/main/java/com/example/mvvvmnews/ui/api/NewsApi.kt
+++ app/src/main/java/com/example/mvvvmnews/ui/api/NewsApi.kt
@@ -1,11 +1,20 @@
 package com.example.mvvvmnews.ui.api
 
+import com.example.mvvvmnews.ui.util.Constants.Companion.API_KEY
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 
 //used to define request
 interface NewsApi {
 
     @GET("v2/top-headlines")
-    suspend fun getBreakingNews()
+    suspend fun getBreakingNews(
+        @Query("country")
+        countryCode: String = "in",
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_KEY
+    )
 }
