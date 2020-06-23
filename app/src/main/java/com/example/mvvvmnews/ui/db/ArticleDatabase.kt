dd83a0efb10dccc017842aebf088b2f2b67777e6
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-23 19:34:08 +0530

    informing database class to use TypeConverters

--- app/src/main/java/com/example/mvvvmnews/ui/db/ArticleDatabase.kt
+++ app/src/main/java/com/example/mvvvmnews/ui/db/ArticleDatabase.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.mvvvmnews.ui.models.Article
 
 @Database(entities = [Article::class], version = 1)
+@TypeConverters(Converters::class)
 abstract class ArticleDatabase : RoomDatabase() {
 
     abstract fun getArticleDao(): ArticleDao
