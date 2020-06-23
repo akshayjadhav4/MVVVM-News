49b992d5a6b05e0d0709dc5c7e3717f469b39d30
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-23 19:26:46 +0530

    created instance of database

--- app/src/main/java/com/example/mvvvmnews/ui/db/ArticleDatabase.kt
+++ app/src/main/java/com/example/mvvvmnews/ui/db/ArticleDatabase.kt
@@ -1,6 +1,8 @@
 package com.example.mvvvmnews.ui.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.mvvvmnews.ui.models.Article
 
@@ -9,4 +11,21 @@ abstract class ArticleDatabase : RoomDatabase() {
 
     abstract fun getArticleDao(): ArticleDao
 
+    companion object {
+        @Volatile
+        private var instance: ArticleDatabase? = null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+            instance ?: createDatabase(context).also {
+                instance = it
+            }
+        }
+
+        private fun createDatabase(context: Context) = Room.databaseBuilder(
+            context.applicationContext,
+            ArticleDatabase::class.java,
+            "article.db"
+        ).build()
+    }
 }
