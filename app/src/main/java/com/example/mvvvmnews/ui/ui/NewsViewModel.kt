b52c847e00124510dd3fa73c93045859af864f8d
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-24 13:05:38 +0530

    setUp getBreakingNews function

--- app/src/main/java/com/example/mvvvmnews/ui/ui/NewsViewModel.kt
+++ app/src/main/java/com/example/mvvvmnews/ui/ui/NewsViewModel.kt
@@ -1,9 +1,41 @@
 package com.example.mvvvmnews.ui.ui
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.mvvvmnews.ui.models.NewsResponse
 import com.example.mvvvmnews.ui.repository.NewsRepository
+import com.example.mvvvmnews.ui.util.Resource
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class NewsViewModel(
     val newsRepository: NewsRepository
-):ViewModel() {
+) : ViewModel() {
+
+    val breakingNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    var breakingNewsPage = 1
+
+    init {
+        getBreakingNews("in")
+    }
+    //used viewModelScope for coroutine
+    fun getBreakingNews(countryCode: String) = viewModelScope.launch {
+
+        //emitting loading response
+        breakingNews.postValue(Resource.Loading())
+
+        //making network resquest
+        val response = newsRepository.getBreakingNews(countryCode, breakingNewsPage)
+        breakingNews.postValue(handleBreakingNewsResponse(response))
+    }
+
+    private fun handleBreakingNewsResponse(response: Response<NewsResponse>): Resource<NewsResponse> {
+        if (response.isSuccessful) {
+            response.body()?.let { resultResponse ->
+                return Resource.Success(resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
 }
