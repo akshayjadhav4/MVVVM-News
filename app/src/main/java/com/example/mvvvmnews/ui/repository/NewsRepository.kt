22357b05dc41e0bb3190a385fe5e270cc9c5d109
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-24 12:30:14 +0530

    defined suspended function getBreakingNews

--- app/src/main/java/com/example/mvvvmnews/ui/repository/NewsRepository.kt
+++ app/src/main/java/com/example/mvvvmnews/ui/repository/NewsRepository.kt
@@ -1,6 +1,12 @@
 package com.example.mvvvmnews.ui.repository
 
+import com.example.mvvvmnews.ui.api.RetrofitInstance
 import com.example.mvvvmnews.ui.db.ArticleDatabase
 
+
+//gets data from da and response
 class NewsRepository(val db: ArticleDatabase) {
+
+    suspend fun getBreakingNews(countryCode: String, pageNumber: Int) =
+        RetrofitInstance.api.getBreakingNews(countryCode, pageNumber)
 }
