843e27ab4c15d2cfd9517bc7cf3de2f2d238b9e5
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-24 17:40:05 +0530

    Article attributes made nullable

--- app/src/main/java/com/example/mvvvmnews/ui/models/Article.kt
+++ app/src/main/java/com/example/mvvvmnews/ui/models/Article.kt
@@ -8,14 +8,14 @@ import java.io.Serializable
 data class Article(
     @PrimaryKey(autoGenerate = true)
     var id: Int? = null,
-    val author: String,
-    val content: String,
-    val description: String,
-    val publishedAt: String,
-    val source: Source,
-    val title: String,
-    val url: String,
-    val urlToImage: String
+    val author: String?,
+    val content: String?,
+    val description: String?,
+    val publishedAt: String?,
+    val source: Source?,
+    val title: String?,
+    val url: String?,
+    val urlToImage: String?
 ):Serializable
 //defined Serializable as this class has  data other than primitive so to tell kotlin
 // and we can pass it easily to navigation args
