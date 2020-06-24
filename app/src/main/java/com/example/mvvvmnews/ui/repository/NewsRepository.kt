1842111bea341aceffd2d8134f653c01821d9ad9
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-24 13:32:07 +0530

    added searchNews function

--- app/src/main/java/com/example/mvvvmnews/ui/repository/NewsRepository.kt
+++ app/src/main/java/com/example/mvvvmnews/ui/repository/NewsRepository.kt
@@ -4,9 +4,12 @@ import com.example.mvvvmnews.ui.api.RetrofitInstance
 import com.example.mvvvmnews.ui.db.ArticleDatabase
 
 
-//gets data from da and response
+//gets data from db and response
 class NewsRepository(val db: ArticleDatabase) {
 
     suspend fun getBreakingNews(countryCode: String, pageNumber: Int) =
         RetrofitInstance.api.getBreakingNews(countryCode, pageNumber)
+
+    suspend fun searchNews(searchQuery: String, pageNumber: Int) =
+        RetrofitInstance.api.searchForNews(searchQuery, pageNumber)
 }
