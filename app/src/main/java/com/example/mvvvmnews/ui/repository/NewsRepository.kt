114df1cd029caeb42ca8a07f6ad2ad52793357d0
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-24 15:39:40 +0530

    added function in repository for room db operations

--- app/src/main/java/com/example/mvvvmnews/ui/repository/NewsRepository.kt
+++ app/src/main/java/com/example/mvvvmnews/ui/repository/NewsRepository.kt
@@ -2,6 +2,7 @@ package com.example.mvvvmnews.ui.repository
 
 import com.example.mvvvmnews.ui.api.RetrofitInstance
 import com.example.mvvvmnews.ui.db.ArticleDatabase
+import com.example.mvvvmnews.ui.models.Article
 
 
 //gets data from db and response
@@ -12,4 +13,11 @@ class NewsRepository(val db: ArticleDatabase) {
 
     suspend fun searchNews(searchQuery: String, pageNumber: Int) =
         RetrofitInstance.api.searchForNews(searchQuery, pageNumber)
+
+
+    suspend fun upsert(article: Article) = db.getArticleDao().upsert(article)
+
+    fun getSavedNews(article: Article) = db.getArticleDao().getAllArticles()
+
+    suspend fun delete(article: Article) = db.getArticleDao().deleteArticle(article)
 }
