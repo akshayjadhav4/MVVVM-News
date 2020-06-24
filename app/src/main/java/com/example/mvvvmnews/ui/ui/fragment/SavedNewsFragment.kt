cfd1341c6f0e42b2b84b6f4baf721ebb08b3eaa0
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-24 14:36:29 +0530

    added onClickListener to navigate to articleFragment with bundle and setup recyclerView

--- app/src/main/java/com/example/mvvvmnews/ui/ui/fragment/SavedNewsFragment.kt
+++ app/src/main/java/com/example/mvvvmnews/ui/ui/fragment/SavedNewsFragment.kt
@@ -3,16 +3,41 @@ package com.example.mvvvmnews.ui.ui.fragment
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mvvvmnews.R
+import com.example.mvvvmnews.ui.adapters.NewsAdapter
 import com.example.mvvvmnews.ui.ui.NewsActivity
 import com.example.mvvvmnews.ui.ui.NewsViewModel
+import kotlinx.android.synthetic.main.fragment_saved_news.*
+import kotlinx.android.synthetic.main.fragment_search_news.*
 
 
 class SavedNewsFragment : Fragment(R.layout.fragment_saved_news) {
     lateinit var viewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
+
+    val TAG = "SavedNewsFragment"
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as NewsActivity).viewModel
+        setUpRecyclerView()
 
+
+        //to open webView
+        newsAdapter.setOnItemClickListener {
+            val bundle = Bundle().apply {
+                putSerializable("article",it)
+            }
+            findNavController().navigate(R.id.action_savedNewsFragment_to_articleFragment,bundle)
+        }
+    }
+
+    private fun setUpRecyclerView() {
+        newsAdapter = NewsAdapter()
+        rvSavedNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
     }
 }
