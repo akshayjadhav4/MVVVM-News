589621cea3a385b5b9f5d4af028466fe7035029f
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-24 18:18:23 +0530

    add app param to constructor

--- app/src/main/java/com/example/mvvvmnews/ui/ui/NewsViewModelFactory.kt
+++ app/src/main/java/com/example/mvvvmnews/ui/ui/NewsViewModelFactory.kt
@@ -1,12 +1,14 @@
 package com.example.mvvvmnews.ui.ui
 
+import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.mvvvmnews.ui.repository.NewsRepository
 
 @Suppress("UNCHECKED_CAST")
-class NewsViewModelFactory(val newsRepository: NewsRepository) : ViewModelProvider.Factory {
+class NewsViewModelFactory(val app: Application, val newsRepository: NewsRepository) :
+    ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return NewsViewModel(newsRepository) as T
+        return NewsViewModel(app,newsRepository) as T
     }
 }
