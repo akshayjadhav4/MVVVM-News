18f124bed70594260a3d4fe2353f6a442990d305
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-24 12:16:03 +0530

    defined viewModel

--- app/src/main/java/com/example/mvvvmnews/ui/ui/fragment/BrakingNewsFragment.kt
+++ app/src/main/java/com/example/mvvvmnews/ui/ui/fragment/BrakingNewsFragment.kt
@@ -1,9 +1,18 @@
 package com.example.mvvvmnews.ui.ui.fragment
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.mvvvmnews.R
+import com.example.mvvvmnews.ui.ui.NewsActivity
+import com.example.mvvvmnews.ui.ui.NewsViewModel
 
 
 class BrakingNewsFragment : Fragment(R.layout.fragment_braking_news) {
 
+    lateinit var viewModel: NewsViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = (activity as NewsActivity).viewModel
+    }
 }
