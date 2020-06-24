751bd57b4ccdaa9f7ef5f9cf1c629a52c8492bb3
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-24 13:56:45 +0530

    constant added for delay

--- app/src/main/java/com/example/mvvvmnews/ui/util/Constants.kt
+++ app/src/main/java/com/example/mvvvmnews/ui/util/Constants.kt
@@ -5,5 +5,6 @@ class Constants {
     companion object {
         const val API_KEY = "03bd6a8afe184fb3aa5bf1c1a959cb13"
         const val BASE_URL = "http://newsapi.org/"
+        const val SEARCH_TIME_DELAY = 500L
     }
 }
