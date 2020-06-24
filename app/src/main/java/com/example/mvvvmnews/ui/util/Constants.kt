1ec4c53c85d464a432e231eae6f822bcd53a595c
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-24 17:42:51 +0530

    constant added for query page response

--- app/src/main/java/com/example/mvvvmnews/ui/util/Constants.kt
+++ app/src/main/java/com/example/mvvvmnews/ui/util/Constants.kt
@@ -6,5 +6,6 @@ class Constants {
         const val API_KEY = "03bd6a8afe184fb3aa5bf1c1a959cb13"
         const val BASE_URL = "http://newsapi.org/"
         const val SEARCH_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
     }
 }
