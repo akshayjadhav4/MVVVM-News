262ca7889076166438ac48b5e12ff39ce7e2590f
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-24 15:54:54 +0530

    created function for room operations in viewModel

--- app/src/main/java/com/example/mvvvmnews/ui/ui/NewsViewModel.kt
+++ app/src/main/java/com/example/mvvvmnews/ui/ui/NewsViewModel.kt
@@ -3,6 +3,7 @@ package com.example.mvvvmnews.ui.ui
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.mvvvmnews.ui.models.Article
 import com.example.mvvvmnews.ui.models.NewsResponse
 import com.example.mvvvmnews.ui.repository.NewsRepository
 import com.example.mvvvmnews.ui.util.Resource
@@ -56,5 +57,15 @@ class NewsViewModel(
             }
         }
         return Resource.Error(response.message())
+    }
+
+    fun saveArticle(article: Article) = viewModelScope.launch {
+        newsRepository.upsert(article)
+    }
+
+    fun getSaveNotes() = newsRepository.getSavedNews()
+
+    fun deleteArticle(article: Article) = viewModelScope.launch {
+        newsRepository.delete(article)
     }
 }
